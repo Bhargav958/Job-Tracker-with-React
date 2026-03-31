5eca46c0b7e46641cf614aeb0ec8e3b21abcf6fc
Bhargav958 <pallebhargav0705@gmail.com>
2026-03-31 16:13:44 +0530

    Updated the register.jsx file

--- job-tracker/src/pages/Register.jsx
+++ job-tracker/src/pages/Register.jsx
@@ -1,9 +1,39 @@
-import React from "react"
+import {useState} from "react"
+import { registerUser } from "../appwrite/auth"
 
 function Register() {
+  //create variable for email and password
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleRegister = async()=>{
+    try {
+      await registerUser(email,password);
+      alert("Registerd Successfully")
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
-    <div>Register</div>
+    <div className="h-screen flex flex-col items-center justify-center gap-4">
+      <input 
+        type="email"
+        placeholder="Email"
+        className="border p-2"
+        onChange={(e)=>setEmail(e.target.value)}
+      />
+      <input 
+        type="password"
+        placeholder="Password"
+        className="border p-2"
+        onChange={(e)=>setPassword(e.target.value)}
+      />
+
+      <button onClick={handleRegister} className="bg-blue-500 text-white px-4 py-2">
+        Register
+      </button>
+    </div>
   )
 }
 
